6c998f81a9fa5d8b203fb7dbd9cff8d7d7d1de6b
Kashmira1605 <kashmirachetan75@gmail.com>
2024-04-17 22:05:41 +0530

    new upload

---
+++ C/Program14.c
@@ -0,0 +1,30 @@
+//Display "Jay Ganesh" 5 times on screen
+
+#include<stdio.h>
+
+//Example of "Iteration"
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    if(iNo < 0)     //Updator
+    {
+        iNo = -iNo;
+    }
+    
+    //      1           2       3
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("Jay Ganesh...\n");      //4
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter number how many times you want to print Jay Ganesh: \n");
+    scanf("%d", &iValue);
+    Display(iValue);
+    
+    return 0;
+}
