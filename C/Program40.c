1db725907e7703ad64f1c17daef44afec31a2d1e
Kashmira1605 <kashmirachetan75@gmail.com>
2024-04-21 21:33:38 +0530

    new upload

---
+++ C/Program40.c
@@ -0,0 +1,29 @@
+// Display the factorial of a number accepted from user
+
+#include<stdio.h>
+
+int CalculateFactorial(int iNo)
+{
+    int iCnt = 0;
+    int ifact = 1;
+    for(iCnt=iNo; iCnt >= 1; iCnt--)
+    {
+        ifact = ifact*iCnt;
+    }
+    return ifact;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number: \n");
+    scanf("%d", &iValue);
+
+    iRet = CalculateFactorial(iValue);
+
+    printf("Factorial of %d is: %d", iValue ,iRet);
+
+    return 0;
+}
